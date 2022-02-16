5437013e68ce477014ca27327289c448dded373e
Diego Fersan <diegofersan@gmail.com>
2022-02-16 19:53:22 -0300

    Initial commit

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div className="flex w-full h-screen justify-center items-center bg-slate-200">
-      <h1 className="text-5xl font-bold bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">Remix Boilerplate</h1>
+      <h1 className="text-5xl bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">DIEGOFERSAN.COM</h1>
     </div>
   );
 }
